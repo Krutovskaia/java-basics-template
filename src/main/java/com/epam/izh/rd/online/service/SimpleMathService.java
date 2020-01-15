62643b788afeaa86e2b97344b78e3102fb862a82
Viktoriia Krutovskaia <viktoriakrutovskaa408@gmail.com>
2020-01-15 23:05:45 +0400

    Implementation of methods in SimpleMathService is written. All tests are passed.

--- src/main/java/com/epam/izh/rd/online/service/SimpleMathService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleMathService.java
@@ -2,108 +2,111 @@ package com.epam.izh.rd.online.service;
 
 public class SimpleMathService implements MathService {
 
-    /**
-     * Метод возвращает 0, если value1 = value2.
-     * Метод возвращает -1, если value1 < value2.
-     * Метод возвращает 1, если value1 > value2.
-     *
-     * Например для (-1, -1) метод должен вернуть 0;
-     * Например для (-3, -1) метод должен вернуть -1;
-     * Например для (3, 1) метод должен вернуть 1;
-     */
     @Override
     public int compare(int value1, int value2) {
-        return -2;
+        return Integer.compare(value1, value2);
     }
 
-    /**
-     * Метод возвращает максимальное число из пары.
-     * Например для списка (-1, 2) метод должен вернуть 2
-     */
     @Override
     public int maxFrom(int value1, int value2) {
-        return -1;
+        return Math.max(value1, value2);
     }
 
-    /**
-     * Метод возвращает максимальное число из переданного массива.
-     * Например для списка {-1, -3, 4, 8, 5, 22, -5} метод должен вернуть 22
-     */
     @Override
     public int maxFrom(int[] values) {
-        return -1;
+        int maxValues = values[0];
+        for (int i = 1; i < values.length; i++) {
+            if (maxValues < values[i]) {
+                maxValues = values[i];
+            }
+        }
+        return maxValues;
     }
 
-    /**
-     * Метод возвращает сумму чисел массива.
-     * Например для списка {-1, -3, 4, 8, 5, 22, -5} метод должен вернуть 30
-     */
     @Override
     public int sum(int[] values) {
-        return -1;
+        int total = 0;
+        for (int value : values) {
+            total = total + value;
+        }
+        return total;
     }
 
-    /**
-     * Метод фильтрует массив, оставляя только четные числа.
-     * Например для списка {-1, -3, 4, 8, 5, 22, 17} метод должен вернуть {4, 8, 22}
-     */
     @Override
     public int[] getEvenDigits(int[] values) {
-        return new int[]{};
+        int evenNums = 0;
+        for (int value : values) {
+            if (value % 2 == 0) {
+                evenNums++;
+            }
+        }
+        int[] found = new int[evenNums];
+        int nums = 0;
+        for (int value : values) {
+            if (value % 2 == 0) {
+                found[nums] = value;
+                nums++;
+            }
+        }
+        return found;
     }
 
-    /**
-     * Метод считает факториал из заданного числа.
-     * Например для числа 5 метод должен вернуть 120.
-     * Факториал 0 должен быть равен 1.
-     */
     @Override
     public long calcFactorial(int initialVal) {
-        return -1L;
+        int result = 1;
+        for (int i = 1; i <= initialVal; i++) {
+            result *= i;
+        }
+        return result;
     }
 
-    /**
-     * Метод возвращает число, которе находится на заданной позиции (счет начинается с нуля) в ряду фибоначчи.
-     *
-     * Ряд фибоначчи - ряд, следующие элементы которого состоят из суммы двух предыдущих.
-     * Ряд начинается 0 и 1.
-     * Пример 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 ...
-     *
-     * Для числа 9 метод должен вернуть 34
-     * Для числа 0 метод должен вернуть 0
-     */
     @Override
     public long calcFibonacci(int number) {
-        return -1L;
+        if (number == 0) {
+            return 0;
+        } else if (number == 1) {
+            return 1;
+        } else {
+            return calcFibonacci(number - 1) + calcFibonacci(number - 2);
+        }
     }
 
-    /**
-     * Метод возвращает отсортированный по возрастанию массив.
-     * Например для массива {-1, -3, 4, 8, 5, 22, -5} метод должен вернуть {-5, -3, -1, 4, 5, 8, 22}
-     */
     @Override
     public int[] sort(int[] values) {
-        return new int[]{};
+        for (int i = values.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (values[j] > values[j + 1]) {
+                    int tmp = values[j];
+                    values[j] = values[j + 1];
+                    values[j + 1] = tmp;
+                }
+            }
+        }
+        return values;
     }
 
-    /**
-     * Метод определяет, является ли заданное число простым.
-     * Простое число - число, которое делится только на 1 и на само себя.
-     *
-     * Например для числа 22 вернется false, а для числа 23 true.
-     */
     @Override
     public boolean isPrimary(int number) {
-        return false;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    /**
-     * Метод возвращает массив, в котором элементы расположены в обратном порядке.
-     *
-     * Например для массива {-1, -3, 4, 8, 5, 22, -5} метод вернет {-5, 22, 5, 8, 4, -3, -1}
-     */
     @Override
     public int[] reverseArray(int[] values) {
-        return new int[]{};
+        int i = 0;
+        int j = values.length - 1;
+        int tmp;
+        while (j > i) {
+            tmp = values[j];
+            values[j] = values[i];
+            values[i] = tmp;
+            j--;
+            i++;
+        }
+        return values;
     }
 }
